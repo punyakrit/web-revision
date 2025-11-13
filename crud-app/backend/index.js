ad83abfaf8f4b0bbe66eadf7e22fcc734d24a08b
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-11-13 17:46:31 +0530

    updated endpoints

--- crud-app/backend/index.js
+++ crud-app/backend/index.js
@@ -2,13 +2,54 @@ const express= require('express')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
+const {z} = require('zod')
+const { todo } = require('./db/db')
 
 
-mongoose.connect(process.env.MONGO_DB)
+const todoSchema = z.object({
+    title: z.string(),
+    description : z.string(),
+    isDone : z.boolean()
+})
+
+app.use(express.json())
+
+
+app.post('/',async (req,res)=>{
+    const {title, description, isDone} = req.body
+    const validate = todoSchema.safeParse({title, description, isDone})
+
+    if(!validate.success){
+        return res.status(411).json({
+            msg: "Wrong input sent"
+        })
+    }
+    await todo.create({
+        title,
+        description,
+        isDone
+    })
+
+    res.json({
+        msg:"Todo Saved"
+    })
+})
+
+app.get('/',async (req,res)=>{
+    const all = await todo.find({})
+    res.json({all})
+})
+
+app.put('/', async(req,res)=>{
+    
+})
 
 
 
 
 
+app.listen(3000,async ()=>{
 
-app.listen(3000)
+    await mongoose.connect('mongodb+srv://admin:wyWWjD7kVASGlKJh@cluster0.gt81chd.mongodb.net/crud-basics')
+    console.log("server started")
+})
